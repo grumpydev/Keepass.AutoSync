b52a88e99820d761ce318ee58da205fb2d1ee429
Steven Robbins <ste.robbins@gmail.com>
2012-09-06 07:48:41 +0100

    Initial commit

---
+++ src/AutoSync/AutoSyncExt.cs
@@ -0,0 +1,115 @@
+﻿namespace AutoSync
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    using KeePass.Forms;
+    using KeePass.Plugins;
+
+    using KeePassLib;
+    using KeePassLib.Interfaces;
+    using KeePassLib.Serialization;
+
+    public class AutoSyncExt : Plugin
+    {
+        private readonly IDictionary<string, FileSystemWatcher> watchers = new Dictionary<string, FileSystemWatcher>();
+
+        private IPluginHost host;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            this.host = host;
+
+            this.host.MainWindow.FileOpened += MainWindowOnFileOpened;
+            this.host.MainWindow.FileClosingPre += MainWindowOnFileClosingPre;
+
+            return true;
+        }
+
+        private void MainWindowOnFileOpened(object sender, FileOpenedEventArgs fileOpenedEventArgs)
+        {
+            if (!fileOpenedEventArgs.Database.IOConnectionInfo.IsLocalFile())
+            {
+                return;
+            }
+
+            this.AddMonitor(fileOpenedEventArgs.Database.IOConnectionInfo.Path);
+        }
+
+        private void MainWindowOnFileClosingPre(object sender, FileClosingEventArgs fileClosingEventArgs)
+        {
+            if (!fileClosingEventArgs.Database.IOConnectionInfo.IsLocalFile())
+            {
+                return;
+            }
+
+            this.RemoveMonitor(fileClosingEventArgs.Database.IOConnectionInfo.Path);
+        }
+
+        private void AddMonitor(string databaseFilename)
+        {
+            if (watchers.ContainsKey(databaseFilename))
+            {
+                return;
+            }
+
+            var path = Path.GetDirectoryName(databaseFilename);
+            var filename = Path.GetFileName(databaseFilename);
+
+            if (filename == null || path == null)
+            {
+                return;
+            }
+
+            var watcher = new FileSystemWatcher(path, filename);
+            watcher.Changed += this.MonitorChanged;
+            watcher.EnableRaisingEvents = true;
+
+            this.watchers.Add(databaseFilename, watcher);
+        }
+
+        private void RemoveMonitor(string databaseFilename)
+        {
+            if (!watchers.ContainsKey(databaseFilename))
+            {
+                return;
+            }
+
+            this.watchers[databaseFilename].Dispose();
+            this.watchers.Remove(databaseFilename);
+        }
+
+        private void MonitorChanged(object sender, FileSystemEventArgs e)
+        {
+            var watcher = sender as FileSystemWatcher;
+
+            if (watcher == null)
+            {
+                return;
+            }
+
+            watcher.EnableRaisingEvents = false;
+
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+
+            this.SyncDatabase(e.FullPath);
+
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void SyncDatabase(string databaseFilename)
+        {
+            var db = new PwDatabase();
+
+            db.Open(IOConnectionInfo.FromPath(databaseFilename), this.host.Database.MasterKey, new NullStatusLogger());
+
+            this.host.Database.MergeIn(db, PwMergeMethod.Synchronize);
+            this.host.MainWindow.RefreshEntriesList();
+
+            db.Close();
+        }
+    }
+}
---
+++ src/AutoSync/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoSync")]
+[assembly: AssemblyDescription("Automatically monitors the database file and make KeePass syncronise with it if it changes - designed for DropBox/Skydrive hosted databases.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Steven Robbins")]
+[assembly: AssemblyProduct("KeePass Plugin")]
+[assembly: AssemblyCopyright("Copyright © Steven Robbins 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("37aef9f2-4c74-4e48-a5a0-339e9b4ed64d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
